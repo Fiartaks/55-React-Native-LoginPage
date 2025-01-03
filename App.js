179d2d36efd09624a320ef65e462a16bae2be26a
fiartaks <fiartaks@gmail.com>
2025-01-03 20:36:55 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Pressable, Image } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const[result, setResult] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+      source={require('./assets/images/unlock.png')}
+      style={styles.image} />
+      <Text style={styles.welcome}>Welcome {result}</Text>
+      <Text>Name</Text>
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={setName}
+        style={styles.textInputStyle}
+      />
+
+      <Text>Last Name</Text>
+      <TextInput
+        placeholder="Enter your last name"
+        onChangeText={setLastName}
+        style={styles.textInputStyle}
+      />
+
+      <Pressable 
+      onPress={()=>setResult( name+''+lastName)}
+
+      style={ ({pressed})=> [{
+        backgroundColor: pressed ? 'gray' : 'blue'
+      }, styles.button]}>
+
+        <Text style={styles.buttonText}>Save</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#67678768",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  textInputStyle: {
+    borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    marginVertical: 10,
+    textAlign: "center",
+    color: "blue",
+  },
+  button: {
+    borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  image:{
+    width: 100,
+    height: 100
+  },
+  welcome: {
+    fontWeight: "bold",
+    fontSize:26,
+  }
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+# 55-React-Native-LoginPage
